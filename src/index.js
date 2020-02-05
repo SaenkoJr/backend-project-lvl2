16e7ceb70e9c7739d28f9d9430d4849f7fcb2c5b
Maksim Saenko <mm.slim.one@gmail.com>
2020-02-05 14:49:20 +0200

    added description

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import commander from 'commander';
 const program = new commander.Command();
 
 export default () => {
-  program.version('0.0.2');
+  program
+    .version('0.0.4');
+
+  program
+    .option('-f, --format [type]', 'output format')
+    .description('Compares two configuration files and shows a difference.')
+    .arguments('<firstConfig> <secondConfig>')
+    .action((firstConfig, secondConfig) => {
+      console.log(firstConfig);
+      console.log(secondConfig);
+    });
+
   program.parse(process.argv);
 };
