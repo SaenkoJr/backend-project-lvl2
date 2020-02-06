3479208ad912fe030e5720f540f772ad490c8276
Maksim Saenko <mm.slim.one@gmail.com>
2020-02-06 21:36:18 +0200

    added diff func

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,5 +1,20 @@
 #!/usr/bin/env node
 
+import program from 'commander';
+
 import gendiff from '..';
 
-gendiff();
+program
+  .version('0.1.0');
+
+program
+  .option('-f, --format [type]', 'output format')
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<firstConfig> <secondConfig>')
+  .action((filepath1, filepath2) => {
+    const diff = gendiff(filepath1, filepath2);
+
+    console.log(diff);
+  });
+
+program.parse(process.argv);
--- src/index.js
+++ src/index.js
@@ -1,19 +1,30 @@
-import commander from 'commander';
+import fs from 'fs';
+import _ from 'lodash';
 
-const program = new commander.Command();
+export default (filepath1, filepath2) => {
+  const before = JSON.parse(fs.readFileSync(filepath1, 'utf-8'));
+  const after = JSON.parse(fs.readFileSync(filepath2, 'utf-8'));
+  const keys = _.union(Object.keys(before), Object.keys(after));
 
-export default () => {
-  program
-    .version('0.0.4');
+  if (_.isEqual(before, after)) {
+    return '';
+  }
 
-  program
-    .option('-f, --format [type]', 'output format')
-    .description('Compares two configuration files and shows a difference.')
-    .arguments('<firstConfig> <secondConfig>')
-    .action((firstConfig, secondConfig) => {
-      console.log(firstConfig);
-      console.log(secondConfig);
-    });
+  const result = keys.map((key) => {
+    if (!_.has(before, key)) {
+      return `  + ${key}: ${after[key]}`;
+    }
 
-  program.parse(process.argv);
+    if (!_.has(after, key)) {
+      return `  - ${key}: ${before[key]}`;
+    }
+
+    if (!_.isEqual(before[key], after[key])) {
+      return `  - ${key}: ${before[key]}\n  + ${key}: ${after[key]}`;
+    }
+
+    return `\n  ${key}: ${before[key]}`;
+  });
+
+  return `{${result.join('\n')}\n}`;
 };
