6557e1d766e6b5f8e70216396926980c1398cf02
Maksim Saenko <mm.slim.one@gmail.com>
2020-02-07 13:18:16 +0200

    added ini parser

--- src/parsers.js
+++ src/parsers.js
@@ -1,7 +1,9 @@
 import yaml from 'js-yaml';
+import ini from 'ini';
 
 export default {
   json: (data) => JSON.parse(data),
   yaml: (data) => yaml.safeLoad(data),
   yml: (data) => yaml.safeLoad(data),
+  ini: (data) => ini.parse(data),
 };
