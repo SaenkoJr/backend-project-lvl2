483cfc1001576cad78d808c5e9863e36ef410c9e
Maksim Saenko <mm.slim.one@gmail.com>
2020-02-07 13:18:23 +0200

    updated tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -3,7 +3,7 @@ import path from 'path';
 
 import gendiff from '../src';
 
-const formats = ['json', 'yml'];
+const formats = ['json', 'yml', 'ini'];
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
